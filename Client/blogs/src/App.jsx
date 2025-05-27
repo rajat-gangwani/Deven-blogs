68aa17edda6a808719004dd962a8d05f40bb4a39
krishAI04 <krishyadav2022@gmail.com>
2025-05-27 20:36:20 +0530

    Update App.jsx

--- Client/blogs/src/App.jsx
+++ Client/blogs/src/App.jsx
@@ -9,7 +9,6 @@ import Blog from "./pages/Blog";
 import BlogCategory from "./pages/BlogCategory";
 import BlogDetail from "./pages/BlogDetail";
 import Login from "./pages/Login";
-
 import AdminDashboard from "./pages/AdminDashboard";
 import Signup from "./pages/Signup";
 import TermsOfService from "./pages/TermsOfService";
@@ -21,13 +20,17 @@ import PrivacyPolicy from "./pages/PrivacyPolicy";
 
 const RootLayout = () => (
   <>
-    <Navbar />
-    <div className="pt-19 min-h-screen flex flex-col justify-between">
-      <div className="flex-grow">
-        <Outlet />
-      </div>
-      <Footer />  {/* Footer appears on all pages */}
-    </div>
+  <div className="dark:bg-[#1C1C1E] bg-[#F5F5F5] transition-colors duration-700 min-h-screen flex flex-col">
+  <Navbar />
+  
+  {/* Add padding top to avoid content hiding behind Navbar */}
+  <main className="flex-grow flex flex-col pt-16"> 
+    <Outlet />
+  </main>
+
+  <Footer /> {/* Footer appears on all pages */}
+</div>
+
   </>
 );
 
@@ -43,7 +46,6 @@ const router = createBrowserRouter(
         { path: "/blog/detail/:slug", element: <BlogDetail /> },
         { path: "/login", element: <Login /> },
         { path: "/signup", element: <Signup /> },
-        
         { path: "/admin", element: <AdminDashboard /> },
         { path: "/terms-&-conditions", element: <TermsOfService /> },
         { path: "/privacy-policy", element: <PrivacyPolicy/> },
