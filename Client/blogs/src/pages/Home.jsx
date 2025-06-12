5cdcc61253b43e5d866a64345981febbf9b7d535
Rajat <rajatgangwani99@gmail.com>
2025-06-12 13:51:37 +0530

    Add black background and move the subscribe section

--- Client/blogs/src/pages/Home.jsx
+++ Client/blogs/src/pages/Home.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import Hero from "../Components/Hero";
-import Blog from "./Blog";
 
 const Home = () => {
   return (
-    <div className="w-full dark:bg-[#1C1C1E] overflow-x-hidden">
+    <div className="w-full dark:bg-black overflow-x-hidden">
       <div className="max-w-screen mx-auto px-4">
         <Hero />
         {/* Optionally include Gallery and Subscribe when needed */}
       </div>
-        <Blog />
     </div>
   );
 };
